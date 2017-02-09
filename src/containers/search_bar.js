687b76b80c7ee5c1d3ec69933e4efe353ba4948e
4xDMG <dajtait87@gmail.com>
2017-02-09 17:37:11 +1000

    Finished API call using Redux Promise middleware

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators} from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state={ term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(event) {
-		console.log(event.target.value);
     this.setState({ term: event.target.value });
 	}
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a 5 day forcast for your favourite cities"
 					className="form-control"
@@ -30,3 +40,9 @@ export default class SearchBar extends Component {
 		);
 	};
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
